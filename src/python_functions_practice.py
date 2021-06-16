53fae344d801efc0344d6ab545ce9af563101ace
Sergio <sergio.eneide@protonmail.com>
2021-06-16 18:30:47 +0100

    message

---
+++ src/python_functions_practice.py
@@ -0,0 +1,27 @@
+def return_10():
+    return 10
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def length_of_string(num1):
+    return 21
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def test_join_string(self):
+    string_1 = "Mary had a little lamb, "
+    string_2 = "its fleece was white as snow"
+    joined_string = join_string( string_1, string_2 )
+    self.assertEqual( "Mary had a little lamb, its fleece was white as snow", joined_string )
+
